b7b541dd34dc3d460b3cfc6e18437815052a8df7
Joseph Ellis <josephellis95@gmail.com>
2024-01-17 13:07:12 -0600

    creating reduser pattern

--- frontend/components/TodoForm.js
+++ frontend/components/TodoForm.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+const CHANGE_LABEL= 'CHANGE_LABE'
+const CHANGE_IS_COMPLETED = 'CHANGE_IS_COMPLETED'
+
+const initialState = {
+  todoLabel: '',
+  todoLabelCompleted: false,
+}
+
+
+const reduser = (state, action) => {
+  switch (action.type) {
+    case CHANGE_LABEL:
+      return {...state, todoLabel: action.playload }
+      case CHANGE_IS_COMPLETED: 
+      return {...state, todoLabelCompleted: action.playload }
+      default: 
+      return state
+  }
+}
+
 
 export default function TodoForm() {
+  const [state, dispatch] = useReducer(reduser, initialState)
   return (
     <form id="todoForm">
       <h3>New Todo Form</h3>
